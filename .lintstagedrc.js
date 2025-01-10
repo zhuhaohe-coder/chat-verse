6e076e49dd1de012bd9befc0485648d867994550
朱豪鹤 <93075751+zhuhaohe-coder@users.noreply.github.com>
2025-01-10 10:56:54 +0800

    chore(other): 修复lint警告

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.{json,md}': ['prettier --write', 'git add'],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{json,md}': ['prettier --write'],
 };
